b788d6e04496cee5d31796a40a9f5cde4e6b8546
nir5113 <nir5113@OZWDLND132-195.win.psu.edu>
2016-09-16 09:03:02 -0400

    added switch statments

--- src/wheeloffortune/WheelOfFortuneApp.java
+++ src/wheeloffortune/WheelOfFortuneApp.java
@@ -7,6 +7,7 @@
 */
 package wheeloffortune;
 
+import java.util.Scanner;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
@@ -15,14 +16,41 @@ public class WheelOfFortuneApp {
   /**
    * @param args the command line arguments
    */
+    
+    
   public static void main(String[] args) {
       
+    String spinWheel = "Spin the wheel";
+    String buyVowel = "Buy a vowel";
+    String solvePuzzle = "Solve the puzzle";    
       
   System.out.println("Please select from the following options:");
-  System.out.println("Spin the wheel");
-  System.out.println("Buy a vowel");
-  System.out.println("Solve the puzzle");
-  System.out.println("Quit the game");
+  System.out.println("Press 1 to Spin the wheel");
+  System.out.println("Press 2 to Buy a vowel");
+  System.out.println("Press 3 Solve the puzzle");
+  System.out.println("Press 4 Quit the game");
+  
+  Scanner sc = new Scanner(System.in);
+  int Selection = sc.nextInt();
+  
+  switch (Selection){
+      case 1:
+          System.out.println("You have chosen to "+spinWheel);
+          break;
+      case 2:
+          System.out.print("You have chosen to "+ buyVowel);
+          break;
+      case 3:
+          System.out.println("You have chosen to "+ solvePuzzle);
+          break;
+      case 4:
+          System.exit(0);
+  }
+  
+  
+  
+  
+  
   
   
     /*
