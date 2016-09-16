36bfb6ce1716bd1146163e5e85350933d420cfea
Nelson4691@gmail.com <nelson4691@gmail.com>
2016-09-15 23:26:51 -0400

    Created print statements

--- src/wheeloffortune/WheelOfFortuneApp.java
+++ src/wheeloffortune/WheelOfFortuneApp.java
@@ -16,10 +16,20 @@ public class WheelOfFortuneApp {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
+      
+      
+  System.out.println("Please select from the following options:");
+  System.out.println("Spin the wheel");
+  System.out.println("Buy a vowel");
+  System.out.println("Solve the puzzle");
+  System.out.println("Quit the game");
+  
+  
+    /*
     JFrame GameFrame = new JFrame ();
     GameFrame.setVisible(true);
     GameFrame.setSize(640,480);
-    GameFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    GameFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);*/
   }
   
 }
