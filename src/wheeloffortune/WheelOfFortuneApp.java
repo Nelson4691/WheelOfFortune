6073fc71e0265946c65bc4bdcc196bed23358240
Nelson4691@gmail.com <nelson4691@gmail.com>
2016-09-29 20:19:43 -0400

    Created a method (numericValidator()) used to check String can be converted to integer Updated the getUserOptionSelection() method to se numericValidator method to check if user option selection is a valid one

--- src/wheeloffortune/WheelOfFortuneApp.java
+++ src/wheeloffortune/WheelOfFortuneApp.java
@@ -24,15 +24,17 @@ private static List<String> gameOptions;           //ArrayList for providing gam
 private static Scanner  userInput;                 //Scanner for user input
 private static String letterGuess;                 //Used to store users guess
 private static String currentWord;                 //Stores the current word user is guessing
-private static int optionSelection;
-private static boolean letterCheck;
-private static int randomValue;
+private static int optionSelection;                //Sotores selection                
+private static int randomValue;                    //Stores the random value
 private static Random randGenerator;
-private static List<String> vocabList;
-private static String wordGuessDisplay=""; 
-private static boolean gameActive = true;
-private static boolean booleanToggleValue = true;
-private static String temporaryCurrentWordHolder;
+private static List<String> vocabList;             //Stores the vocab words
+private static String currentpuzzleDisplayed="";   //Dipslays and stores current puzzle and user correct guess       
+private static boolean gameActive = true;          //used to check if game is active    
+private static boolean booleanToggleValue = true;  //Used to check guess value 
+private static String temporaryCurrentWordHolder;  //Used to temporary hold word 
+private static String currentWedge;                //Will be used to include the wedges 
+private static int currentPlayerScore = 0;                    //Used to keep track of player score
+
     
   public static void main(String[] args) {
       //created two methods which add the game options which are included in an
@@ -51,6 +53,7 @@ private static String temporaryCurrentWordHolder;
       createPuzzleSpaces(); //printes out required word space
       while(gameActive){
       displayCurrentPuzzle(); 
+      displayPlayerScore();
       displayGameOptions();
       getUserOptionSelection(); //prompts user for option selection
       checkUserSelection();
@@ -63,6 +66,28 @@ private static String temporaryCurrentWordHolder;
       randomValue = randGenerator.nextInt(wedgeOptions.size()-1)+0;
       return randomValue;
     }
+    private static void updatePlayerScore(){
+    if(numericValidator(currentWedge) == true)
+        currentPlayerScore = currentPlayerScore + Integer.parseInt(currentWedge);    
+    }
+    /*Used to test if value is not numberic*/
+    public static boolean numericValidator(String str)  
+    {  
+        try  
+    {  
+        int value = Integer.parseInt(str);  
+    }  
+        catch(NumberFormatException error)  
+    {  
+        return false;  
+    }  
+        return true;  
+    }
+    private static void displayPlayerScore(){
+        System.out.println("Your current Score: " + currentPlayerScore + "\n");
+
+    }
+    
     //Adds selected options to hashmap
     private static  void addSelectedGameOptions(){
         selectedOptions = new HashMap<>();
@@ -87,41 +112,44 @@ private static String temporaryCurrentWordHolder;
         gameOptions.add("Enter choice:");
     }
     private static void displayGameOptions(){
-        for(int i = 0; i < gameOptions.size(); i++){
-        System.out.println(gameOptions.get(i));
-      }
+        gameOptions.stream().forEach((Options) ->
+        {
+            System.out.println(Options);
+        });
     }
     //adds and stores wedge options
     private static void addwedgeOptions(){
         wedgeOptions = new ArrayList<>();
-        wedgeOptions.add("$5000");
-        wedgeOptions.add("$600");
-        wedgeOptions.add("$500");
-        wedgeOptions.add("$300");
-        wedgeOptions.add("$500");
-        wedgeOptions.add("$800");
-        wedgeOptions.add("$550");
-        wedgeOptions.add("$400");
-        wedgeOptions.add("$300");
-        wedgeOptions.add("$900");
-        wedgeOptions.add("$500");
-        wedgeOptions.add("$300");
-        wedgeOptions.add("$900");
+        wedgeOptions.add("5000");
+        wedgeOptions.add("600");
+        wedgeOptions.add("500");
+        wedgeOptions.add("300");
+        wedgeOptions.add("500");
+        wedgeOptions.add("800");
+        wedgeOptions.add("550");
+        wedgeOptions.add("400");
+        wedgeOptions.add("300");
+        wedgeOptions.add("900");
+        wedgeOptions.add("500");
+        wedgeOptions.add("300");
+        wedgeOptions.add("900");
         wedgeOptions.add("Bankrupt");
-        wedgeOptions.add("$600");
-        wedgeOptions.add("$400");
-        wedgeOptions.add("$300");
+        wedgeOptions.add("600");
+        wedgeOptions.add("400");
+        wedgeOptions.add("300");
         wedgeOptions.add("Lose a Turn");
-        wedgeOptions.add("$800");
-        wedgeOptions.add("$350");
-        wedgeOptions.add("$450");
-        wedgeOptions.add("$700");
-        wedgeOptions.add("$300");
-        wedgeOptions.add("$600");
+        wedgeOptions.add("800");
+        wedgeOptions.add("350");
+        wedgeOptions.add("450");
+        wedgeOptions.add("700");
+        wedgeOptions.add("300");
+        wedgeOptions.add("600");
     }       
     private static void addVocabList(){
         vocabList = new ArrayList<>();
         vocabList.add("T H E  Q U I C K  B R O W N  F O X  J U M P S  O V E R  T H E  L A Z Y  D O G");
+        vocabList.add("C O D E I N G  I S  F U N");
+        vocabList.add("Y O U  W I L L  N E V E R  W I N");
 }
     private static void getVocabWord(){
         randGenerator = new Random(); //what if you instantiating it everytime? Is that safe?
@@ -143,27 +171,51 @@ private static String temporaryCurrentWordHolder;
         for (int i = 0 ; i < currentWord.length(); i++){   //Loop runs as many times as current word
             if(currentWord.charAt(i) == ' '){ //           //Checks if character at certain persition has an empty space
                 //System.out.print(" ");
-                wordGuessDisplay = wordGuessDisplay + " "; //Updates the hint to includes a blank space
+                currentpuzzleDisplayed = currentpuzzleDisplayed + " "; //Updates the hint to includes a blank space
         }
             else
                 //System.out.print("-");
-                wordGuessDisplay = wordGuessDisplay + "_"; //if it doesn't have a blank space then it includes an Underscore
+                currentpuzzleDisplayed = currentpuzzleDisplayed + "_"; //if it doesn't have a blank space then it includes an Underscore
             }
     }
     private static void displayCurrentPuzzle(){
-        System.out.println("\n"+wordGuessDisplay);
+        System.out.println("\n"+currentpuzzleDisplayed);
         System.out.print('\n');
     }
     private static void getUserOptionSelection(){
-        userInput = new Scanner(System.in);
-        optionSelection = Integer.parseInt(userInput.next()) ;
+        
+       userInput = new Scanner(System.in);
+       String optionValidator = userInput.next();               //variable checks for option selection
+        while(numericValidator(optionValidator) == false){           
+            System.out.println("Please enter a valid option");
+            optionValidator = userInput.next();
+         
+        }
+        optionSelection = Integer.parseInt(optionValidator) ;
+        
+        /*boolean Validator = true;
+        
+       while(Validator){
+           
+           try{
+           userInput = new Scanner(System.in);
+           optionSelection = Integer.parseInt(userInput.next());
+           }
+           catch(NumberFormatException error){
+               System.out.println("Error, not a valid selection. Please try again.");
+               if (optionSelection >0){ Validator = false;}
+           }
+       }
+        //optionSelection = Integer.parseInt(userInput.next()) ;
+        
+      */    
     }
     private static void checkWordGuess(){
         for (int i = 0 ; i < currentWord.length(); i++){                     //Loop runs as many times as current word
             if(currentWord.charAt(i) ==  letterGuess.charAt(0)){             // checks if the current word matches the letter guessed
-                StringBuilder myName = new StringBuilder(wordGuessDisplay);  //if matches bulder is instaniated out of currentworddisplay Ex.(__ _ _)
+                StringBuilder myName = new StringBuilder(currentpuzzleDisplayed);  //if matches bulder is instaniated out of currentworddisplay Ex.(__ _ _)
                 myName.setCharAt(i,letterGuess.charAt(0) );                  //since the characters in myname are mutuable ill change it using setchar(string posiition, character)
-                wordGuessDisplay = myName.toString();                        // wordGuessDisplay will then be updated = myName
+                currentpuzzleDisplayed = myName.toString();                        // currentpuzzleDisplayed will then be updated = myName
               }
        
         }
@@ -172,12 +224,14 @@ private static String temporaryCurrentWordHolder;
     private static void checkUserSelection(){
         switch (optionSelection){
             case 1:
-                System.out.print("You Landed on: "+wedgeOptions.get(getWedge())+"\n");
+                currentWedge = wedgeOptions.get(getWedge());
+                updatePlayerScore();
+                System.out.print("You Landed on: "+currentWedge+"\n");
                 System.out.print("Enter a letter to guess: ");
-                letterGuess  =(userInput.next());
+                letterGuess  =(userInput.next().toUpperCase());
                 while(!Character.isLetter(letterGuess.charAt(0))){
                     System.out.println("Invalid!\n Please guess a letter from A-Z");
-                    letterGuess  =(userInput.next());
+                    letterGuess  =(userInput.next().toUpperCase());
                 }
                 checkWordGuess();       //Checks word guess if guess is correct it updates the display
                 break;
@@ -193,14 +247,15 @@ private static String temporaryCurrentWordHolder;
                 gameActive = false;
                 break;
             case 8:
-                if(booleanToggleValue){
-                   temporaryCurrentWordHolder = wordGuessDisplay;  // putes your guesses in temporary holder 
-                   wordGuessDisplay = currentWord;                 // makes orginally guess holder display full word
-                   booleanToggleValue = false;
+                
+                if(booleanToggleValue){                                  /* The if statement allows user to see the word if 8 is pressed.*/
+                   temporaryCurrentWordHolder = currentpuzzleDisplayed;  // putes your guesses in temporary holder 
+                   currentpuzzleDisplayed = currentWord;                 // makes the puzzle displayed equal to the actual word guess. So user can see the full word
+                   booleanToggleValue = false;                           // makes the value false so that next time user presses 8 the else if statment runs
                }
-                else if(!booleanToggleValue){
-                    wordGuessDisplay = temporaryCurrentWordHolder;
-                    booleanToggleValue = true;
+                else if(!booleanToggleValue){                            // If the variable is not true then means the user doesn't want to see word guess
+                    currentpuzzleDisplayed = temporaryCurrentWordHolder; // The temporary variable holder sends the word back to the variable that is displayed to user   
+                    booleanToggleValue = true;                           // toggle is now set to true so that user can select to see the word next time presses 8
                 
                 }
             break;
